60b82bb4f125273e5235328b618cf33e2e163b28
Ekaterina <nataveres@gmail.com>
2020-03-25 17:51:07 +0100

    Finished all except zero-part)

--- object.js
+++ object.js
@@ -57,11 +57,67 @@ const object = {
       return arr;
     }
   },
-  negativeStrings: function() {},
-  positiveStrings: function() {},
-  zeroStrings: function() {},
-  numberyAsNumbers: function() {},
-  NaNyAsNumbers: function() {},
-  sumOfNumbery: function() {},
-  sumOfNaNy: function() {}
+  negativeStrings: function() {
+    return this.numberyStrings.filter(el => Number(el) < 0);
+    /*
+    this.numberyStrings.filter(el => {
+      Number(el) < 0;
+    });*/
+  },
+  positiveStrings: function() {
+    return this.numberyStrings.filter(el => Number(el) > 0 || el == "");
+    /*let arr = [];
+    this.numberyStrings.forEach(el => {
+      if (Number(el) > 0) {
+        arr.push(el);
+      }
+      return arr; //why return undefined
+    });*/
+  },
+  zeroStrings: function() {
+    if (this.numberyStrings.length == 0) {
+      return this.numberyStrings;
+    } else {
+      //this.numberyStrings.filter(el => Number(el) === 0 || Number(el) === -0 || el === "");
+      let arr = [];
+      this.numberyStrings.forEach(el => {
+        if (el => Number(el) === 0 || Number(el) === -0 || el === "") {
+          arr.push(el);
+        }
+      });
+      return arr;
+    }
+  },
+  numberyAsNumbers: function() {
+    if (this.numberyStrings.length == 0) {
+      return this.numberyStrings;
+    } else {
+      let arr = [];
+      this.numberyStrings.forEach(el => arr.push(Number(el)));
+      return arr;
+    }
+  },
+  NaNyAsNumbers: function() {
+    if (this.NaNyStrings.length == 0) {
+      return this.NaNyStrings;
+    } else {
+      let arr = [];
+      this.NaNyStrings.forEach(el => arr.push(Number(el)));
+      return arr;
+    }
+  },
+  sumOfNumbery: function() {
+    if (this.numberyStrings.length == 0) {
+      return 0;
+    } else {
+      return this.numberyStrings.reduce((a, b) => Number(a) + Number(b), 0);
+    }
+  },
+  sumOfNaNy: function() {
+    if (this.NaNyStrings.length == 0) {
+      return 0;
+    } else {
+      return this.NaNyStrings.reduce((a, b) => Number(a) + Number(b), 0);
+    }
+  }
 };
